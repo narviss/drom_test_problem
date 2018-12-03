8b66476f7333564228ce4f20b0ceb0cf4b217cc1
Vadim Sheludko <narviss@bk.ru>
2018-12-03 18:51:26 +1000

    Формы входа и регистрации

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 
 header('Content-Type: text/html; charset=utf-8');
 require('route.php');
+include('category/main/head.html');
 new Router();
+include('category/main/footer.html');
 ?>
--- route.php
+++ route.php
@@ -4,12 +4,12 @@ final class Router {
     public function __construct(){
         switch ($_GET['action']){
             case 'auth':
-                include("category/auth/auth_form.php");
+                //include("category/auth/auth_form.php");
                 include("category/auth/auth_form.html");
                 break;
             case 'reg':
                 include("category/reg/reg_form.html");
-                include("category/reg/reg_form.php");
+                //include("category/reg/reg_form.php");
                 break;
             default:
                 include("category/main/index.html");
